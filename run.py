06bca05097c1d38e0b39cb2655cf7712d3c85d6c
Saikou Gassama <gassamasaikou@yahoo.com>
2023-05-25 07:24:50 +0000

    Add question and answer if else statements

--- run.py
+++ run.py
@@ -1,1 +1,45 @@
-print("Welcome to my quiz game!")
+print("Welcome to my Quiz Game!")
+
+playing = input("Do you want to play ? \n")
+
+if playing != "yes":
+    quit()
+print("Great! Lets playyyyyy")
+
+
+answer = input("What is the capital city of France ?")
+
+if answer == "Paris":
+    print('Correct!')
+else:
+    print('Incorrect Answer!')
+
+answer = input("What is the capital city of England ?")
+
+if answer == "London":
+    print('Correct!')
+else:
+    print('Incorrect Answer!')
+
+answer = input("What is the capital city of Norway ?")
+
+if answer == "Oslo":
+    print('Correct!')
+else:
+    print('Incorrect Answer!')
+
+answer = input("What is the capital city of Italy ?")
+
+if answer == "Rome":
+    print('Correct!')
+else:
+    print('Incorrect Answer!')
+
+answer = input("What is the capital city of sweden ?")
+
+if answer == "Stockholm":
+    print('Correct!')
+else:
+    print('Incorrect Answer!')
+
+
